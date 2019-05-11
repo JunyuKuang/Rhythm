3d7fe1b92fcc2e97c24c5a9ddc8a1efd61dbf24b
Jonny Kuang <lightscreen.app@gmail.com>
2019-05-11 23:06:01 +0800

    Initial Commit

---
+++ AppleMusicLyrics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMusicLyrics
+//
+//  Created by Jonny Kuang on 5/11/19.
+//  Copyright © 2019 Jonny Kuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
