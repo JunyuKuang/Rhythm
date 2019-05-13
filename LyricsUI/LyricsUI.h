f45d05e01c3d1729290aa52cf960df96549de482
Jonny Kuang <lightscreen.app@gmail.com>
2019-05-13 15:37:19 +0800

    Configure LyricsUI framework

---
+++ LyricsUI/LyricsUI.h
@@ -0,0 +1,19 @@
+//
+//  LyricsUI.h
+//  LyricsUI
+//
+//  Created by Jonny Kuang on 5/13/19.
+//  Copyright © 2019 Jonny Kuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LyricsUI.
+FOUNDATION_EXPORT double LyricsUIVersionNumber;
+
+//! Project version string for LyricsUI.
+FOUNDATION_EXPORT const unsigned char LyricsUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LyricsUI/PublicHeader.h>
+
+
