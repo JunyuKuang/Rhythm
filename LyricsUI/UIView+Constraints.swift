195e1c829619451dc676a5dbdfd32b75166ca54d
Jonny Kuang <lightscreen.app@gmail.com>
2019-05-18 16:26:12 +0800

    GNU General Public License

--- LyricsUI/UIView+Constraints.swift
+++ LyricsUI/UIView+Constraints.swift
@@ -1,8 +1,21 @@
 //
 //  UIView+Constraints.swift
 //
-//  Created by Jonny on 3/3/17.
-//  Copyright © 2017 Jonny. All rights reserved.
+//  Rhythm <https://github.com/JunyuKuang/Rhythm>
+//  Copyright (C) 2019  Junyu Kuang
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #if os(iOS)
--- Rhythm/LocationManager.swift
+++ Rhythm/LocationManager.swift
@@ -1,8 +1,21 @@
 //
 //  LocationManager.swift
 //
-//  Created by Jonny on 5/22/18.
-//  Copyright © 2018 Junyu Kuang <lightscreen.app@gmail.com>. All rights reserved.
+//  Rhythm <https://github.com/JunyuKuang/Rhythm>
+//  Copyright (C) 2019  Junyu Kuang
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import CoreLocation
