f881c08e422c0cb0a1fd3d79e81457721cb52f36
JunyuKuang <lightscreen.app@gmail.com>
2020-02-29 19:29:11 +0800

    Update copyright note

--- LyricsProvider/LyricsProvider.h
+++ LyricsProvider/LyricsProvider.h
@@ -5,7 +5,7 @@
 //  Created by Junyu Kuang on 5/12/19.
 //
 //  Rhythm <https://github.com/JunyuKuang/Rhythm>
-//  Copyright (C) 2019  Junyu Kuang
+//  Copyright (C) 2019-2020  Junyu Kuang
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
--- LyricsUI/AlbumArtworkViewController.swift
+++ LyricsUI/AlbumArtworkViewController.swift
@@ -2,7 +2,7 @@
 //  AlbumArtworkViewController.swift
 //
 //  Rhythm <https://github.com/JunyuKuang/Rhythm>
-//  Copyright (C) 2019  Junyu Kuang
+//  Copyright (C) 2019-2020  Junyu Kuang
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
